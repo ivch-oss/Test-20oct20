0ff9e65e4d3ecbb933343ea8d7a235677f8c89aa
ivch-oss <iv.ch@icloud.com>
2020-10-21 10:57:49 +0300

    Добавьте файлы проекта.

---
+++ Test 20oct20/Test 20oct20.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 3, b = 5, c = 7;
+    a = b; cout << a << endl;
+    b = a; cout << b << endl;;
+    c = a + 1;
+    cout << c << endl;
+    c = a + (b = 5);
+    cout << c;
+    return 0;
+}
